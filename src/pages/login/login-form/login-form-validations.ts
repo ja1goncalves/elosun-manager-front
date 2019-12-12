1f76ce2bd11c3b14d77404c1efaecba595379bf8
valdery-mangue3 <valderyalves@mangue3.com>
2019-12-12 13:20:48 -0300

    :nail_care: Deixa a validação do login como um email
    Signed-off-by: valdery-mangue3 <valderyalves@mangue3.com>

--- src/pages/login/login-form/login-form-validations.ts
+++ src/pages/login/login-form/login-form-validations.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
 export const loginValidations = yup.object().shape({
-    login: yup.string().required('Insira o login'),
+    login: yup.string().required('Insira o login').email('Insira um email válido'),
     password: yup.string().required('Insira a senha'),
 });
