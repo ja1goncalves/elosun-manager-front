2c96ce473bd5058a9d0b782d747de47fb54a2c77
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2020-01-06 14:26:31 -0300

    :nail_care: Diminui o padding do links de navegação da sidebar
    - Diminui o padding top e bottom dos links de navegação da sidebar
    - Adiciona uma largura fixa nos ícones de navegação no sidebar, para todos ficarem alinhados
    
    Signed-off-by: Valdery Alves Paes Junior <valdery.jur@gmail.com>

--- src/components/header-left/nav-link/nav-link-styles.ts
+++ src/components/header-left/nav-link/nav-link-styles.ts
@@ -5,7 +5,11 @@ export const StyledNavLink = styled(NavLink)`
     color: #fff;
     transition: background-color 0.3s;
     border-radius: 6px;
-    padding: 10px;
+    padding: 5px 10px;
+    display: flex;
+    align-items: center;
+    flex-flow: row nowrap;
+
     :hover {
         color: inherit;
         text-decoration: none;
@@ -14,5 +18,11 @@ export const StyledNavLink = styled(NavLink)`
     &.active {
         background-color: #fff;
         color: ${({ theme }) => theme.blue.terciaryColor};
+    }
+    .sidebar-icon-container {
+        width: 25px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 `;
--- src/components/sidebar/sidebar-component.tsx
+++ src/components/sidebar/sidebar-component.tsx
@@ -3,20 +3,20 @@ import { StyledSidebar, StyledCloseSidebarIcon } from './sidebar-styles';
 import { ProfilePictureComponent } from '../header-left/profile-picture';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { Redirect } from 'react-router';
+import { Redirect, useHistory } from 'react-router';
 import { NavMenuComponent } from './nav-menu';
 import { StyledLogoNav } from '../header-left/header-styles';
 import { HeaderUpContext } from '../header-up/header-up-context';
 
 export default () => {
     const { openSideMenu, setOpenSideMenu } = useContext(HeaderUpContext);
+    const history = useHistory();
 
     const [sidebarWidth, setSidebarWidth] = useState<number>(0);
-    const [goToLogin, setGoToLogin] = useState<boolean>(false);
 
     const logoffUser = () => {
         localStorage.removeItem('auth');
-        setGoToLogin(true);
+        history.push('/login');
     }
 
     useEffect(() => {
@@ -31,42 +31,39 @@ export default () => {
     }, []);
 
     return (
-        <>
-            {goToLogin && <Redirect to="/login" />}
-            <StyledSidebar id="sidebar-mobile" sidebarWidth={sidebarWidth} showSideBar={openSideMenu} className="d-block d-lg-none position-fixed">
-                <div className="container-fluid h-100 d-flex flex-column">
-                    <StyledLogoNav className="row d-flex justify-content-center align-items-center">
-                        <StyledCloseSidebarIcon
-                            onClick={() => setOpenSideMenu(false)}
-                            icon={faArrowLeft} />
-                        <p className="ml-3 mb-0 logo-text">
-                            <b>Elo</b>sun
+        <StyledSidebar id="sidebar-mobile" sidebarWidth={sidebarWidth} showSideBar={openSideMenu} className="d-block d-lg-none position-fixed">
+            <div className="container-fluid h-100 d-flex flex-column">
+                <StyledLogoNav className="row d-flex justify-content-center align-items-center">
+                    <StyledCloseSidebarIcon
+                        onClick={() => setOpenSideMenu(false)}
+                        icon={faArrowLeft} />
+                    <p className="ml-3 mb-0 logo-text">
+                        <b>Elo</b>sun
+                    </p>
+                </StyledLogoNav>
+                <hr style={{ backgroundColor: '#aaa' }} className="w-100 mt-0" />
+                <aside className="row d-flex align-items-center justify-content-start flex-column">
+                    <ProfilePictureComponent imgUrl={require('../../assets/imgs/link.jpg')} />
+                    <div className="d-flex flex-column align-items-center mt-2">
+                        <p style={{ fontSize: 12 }} className="mb-0 text-center">
+                            Valdery Alves Paes Jr<br/>
+                            <b>Administrador</b>
                         </p>
-                    </StyledLogoNav>
-                    <hr style={{ backgroundColor: '#aaa' }} className="w-100 mt-0" />
-                    <aside className="row d-flex align-items-center justify-content-start flex-column">
-                        <ProfilePictureComponent imgUrl={require('../../assets/imgs/link.jpg')} />
-                        <div className="d-flex flex-column align-items-center mt-2">
-                            <p style={{ fontSize: 12 }} className="mb-0 text-center">
-                                Valdery Alves Paes Jr<br/>
-                                <b>Administrador</b>
-                            </p>
-                        </div>
-                    </aside>
-                    <hr style={{ backgroundColor: '#aaa' }} className="w-100" />
-                    <nav className="row">
-                        <NavMenuComponent setShowSidebar={setOpenSideMenu} />
-                    </nav>
-                    <hr style={{ backgroundColor: '#aaa' }} className="w-100" />
-                    <nav className="container-fluid">
-                        <ul className="row d-flex justify-content-center">
-                            <li className="row w-100 d-flex justify-content-start align-items-center flex-row p-2 mx-0" onClick={logoffUser}>
-                                <FontAwesomeIcon className="mr-3" icon={faSignOutAlt} /> Sair
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </StyledSidebar>
-        </>
+                    </div>
+                </aside>
+                <hr style={{ backgroundColor: '#aaa' }} className="w-100" />
+                <nav className="row">
+                    <NavMenuComponent setShowSidebar={setOpenSideMenu} />
+                </nav>
+                <hr style={{ backgroundColor: '#aaa' }} className="w-100" />
+                <nav className="container-fluid">
+                    <ul className="row d-flex justify-content-center">
+                        <li className="row w-100 d-flex justify-content-start align-items-center flex-row p-2 mx-0" onClick={logoffUser}>
+                            <FontAwesomeIcon className="mr-3" icon={faSignOutAlt} /> Sair
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </StyledSidebar>
     )
 }
