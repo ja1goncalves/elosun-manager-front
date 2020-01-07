42e4d846080286966dffacc092a9526c46f88e23
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2020-01-06 21:26:38 -0300

    :sparkles: Adiciona rotas de estação
    - 'getDistributorData' para coletar os dados do distribuidor
    - 'tableData' coleta as estações paginando
    
    Signed-off-by: Valdery Alves Paes Junior <valdery.jur@gmail.com>

--- src/services/station/station.service.ts
+++ src/services/station/station.service.ts
@@ -11,17 +11,21 @@ class StationService implements ITableData {
         this.http.interceptors.request.use(request => addAuthToken(request));
     }
 
+    async getDistributorData(distriibutorId: string) {
+        return this.http.get(`admin/distributors/${distriibutorId}`).then(res => Promise.resolve(res.data));
+    }
+
     /**
      * @description 
      * @param {TableDataParams} param0
      *  - page é a página
-     *  - custom é o id da estação 
+     *  - custom é o id do distribuidor
      */
     async tableData({ page, custom }: TableDataParams): Promise<PaginateDatabaTable> {
         if (page)
-            return this.http.get(`/admin/distributors/stations/${custom}?page=${page}`).then(res => res.data);
+            return this.http.get(`/admin/stations?distributor_id=${custom}&page=${page}`).then(res => res.data);
         else
-            return this.http.get(`/admin/distributors/stations/${custom}`).then(res => res.data);
+            return this.http.get(`/admin/stations?distributor_id=${custom}`).then(res => res.data);
     }
 }
 
